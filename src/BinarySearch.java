aaff844b5ffbf52f0fe619f10698bd5f2dce22dd
Ganesh Khot <ganeshaakhot@gmail.com>
2023-04-06 16:21:52 +0530

    Binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -36,6 +36,25 @@ public class BinarySearch {
     }
 
 
+//    Input: letters = ["c","f","j"], target = "a"
+//    Output: "c"
+//    Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+//    https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+    static char nextGreatestLetter(char [] ch , char x){
+        int low = 0;
+        int high = ch.length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (x < ch[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return ch[low % ch.length];
+    }
 
     public static void main(String[] args) {
         int [] a = {11,23,34,48,52,66};
