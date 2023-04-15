68c52c4dac753063d9abf1134f9c01ca2b42e542
Ganesh Khot <ganeshaakhot@gmail.com>
2023-04-15 13:27:13 +0530

    Arrays

--- src/Arrays/findInMountonArray.java
+++ src/Arrays/findInMountonArray.java
@@ -1,9 +1,56 @@
 package Arrays;
 
 public class findInMountonArray {
-    public int findInMountainArray(int target, MountainArray mountainArr) {
-        int peek =
+    int search(int[] arr, int target) {
+        int peak = peekElement(arr);
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        // try to search in second half
+        return orderAgnosticBS(arr, target, peak+1, arr.length - 1);
     }
 
-    public int peekElement
+    public int peekElement(int a[]) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (a[mid] > a[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public int orderAgnosticBS(int[] a, int target , int low , int high) {
+
+        boolean isSorted = a[low] < a[high];
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] == target){
+                return mid;
+            }
+
+            if (isSorted) {
+                if (target < a[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > a[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
